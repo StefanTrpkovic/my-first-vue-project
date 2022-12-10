bb4ded4c02e1443efc9854c38783e2997bd75f22
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-12-10 19:18:39 +0100

    End of episode 18

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
+<!-- eslint-disable prettier/prettier -->
 <script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+import TheWelcome from "@/components/TheWelcome.vue";
+
+setTimeout(() => {
+  message = "I have been changed"
+}, 2000)
+
+// eslint-disable-next-line no-undef
+let message = $ref("Hello World");
+
+const doSomething = () => {
+  alert('do something')
+}
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    {{ message }}
+    <p>
+      <input type="text" v-model="message" />
+      <button v-on:click="doSomething">Click ME</button>
+    </p>
   </main>
 </template>
